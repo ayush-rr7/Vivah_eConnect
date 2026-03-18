b7a56656e20f96623e4149886da743edec58fcbd
Ayush Ranjan <ayushranjan6976.com>
2026-03-18 10:09:51 +0530

    remove unecssary code

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,9 @@
-
-// import express from 'express';
 import http from 'http'
 import app from './src/app.js'
 import initSocket from './src/sockets/socket.js';
 import { Server } from "socket.io";
 const server = http.createServer(app);
 
-// const io = new Server(server);
-
-// io.on("connection", (socket)=>{
-//    console.log("connected");
-// });
 
 initSocket(server);
 
