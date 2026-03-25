0c49db2bf86f617fec9efdcf26279dcdd45b0879
ayush-rr7 <ayushranjan6976@gmail.com>
2026-03-25 20:37:21 +0530

    Update socket.js

--- frontend/src/api/socket.js
+++ frontend/src/api/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 import { getActiveProfileId } from "../utils/getActiveProfile";
-export const socket = io("http://localhost:3002", {
+const API= import.meta.env.VITE_API_URL
+export const socket = io(API, {
     auth: {
     profileId: getActiveProfileId()
   },
