9c617c1d1b784e4e015cb9bb23b9ae8cfe24b5b2
Ayush Ranjan <ayushranjan6976.com>
2026-03-25 17:26:34 +0530

    Production ready: connected frontend to backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,8 +2,9 @@
 
 import axios from "axios";
 
+const API= import.meta.env.VITE_API_URL
 const api = axios.create({
-  baseURL: "http://localhost:3002",
+  baseURL:API,
   withCredentials: true, // send cookies automatically
   
 });
